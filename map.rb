31c771fd282407f4878795602503aff8353a8e15
T L <t>
2015-12-14 21:56:44 -0700

    more map parsing stuff

--- map.rb
+++ map.rb
@@ -4,6 +4,8 @@ require "FileUtils"
 die = ->(error){puts error; Kernel.exit -1}
 
 USED_MAPS = [:political, :population, :military]
+MAP_COORDS = {x: 180, y: 90}
+
 def territory(color)
 	#check to see all the files are here
 	USED_MAPS.each do |map| 
@@ -13,6 +15,8 @@ def territory(color)
 	end
 
 	#if all the files are there start parsing the maps
+	territoryTable = {}
 	USED_MAPS.each do |map|
+		mapFile = ChunkyPNG::Image.from_file("#{map.to_s}.png")
 		if map == :political 
 
