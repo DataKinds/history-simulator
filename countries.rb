be5173b621e5fd1a520f9f6b33e9612941599383
Tristan <TristanBomb@gmail.com>
2015-12-14 21:50:13 -0700

    flags, countries.rb

---
+++ countries.rb
@@ -0,0 +1,9 @@
+    require "country"
+
+all_countries = []
+
+usa = Country.new("United States", [:democracy, :conservative, :capitalist, :northern_america])
+usa.name
+countries.push(usa)
+puts usa.name
+puts usa.tags
