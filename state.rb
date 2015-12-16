7032ee49d5c7d440fae3051a3f5fdf99369572bc
T L <t>
2015-12-16 15:46:29 -0700

    created state.rb skeleton

---
+++ state.rb
@@ -0,0 +1,20 @@
+require "yaml"
+
+YAML.load_file("countries.yml")
+
+#loads all the maps and country files and etc.
+#returns a hash object containing everything
+def load()
+
+end
+
+#takes the hash object returned by load and saves
+#it to all the corresponding files
+def save()
+
+end
+
+#moves the world forward
+def tick()
+
+end
